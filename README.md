afc601a12148ae6acc28e1dc1831e99d8f584241
Tyler Johnson <tyler@tylerjohnson.me>
2016-01-05 17:27:25 -0700

    added readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Superfast TodoMVC Example
+
+This is a [TodoMVC](http://todomvc.com/) example for [Superfast](http://ghub.io/superfast) and [Temple](http://ghub.io/templejs).
+
+To run locally, you will need the Superfast CLI tool installed, as well as local node modules installed. Then just run the Superfast command in the directory to start a local server. The app will be available at <http://localhost:3000>.
+
+```bash
+$ npm i superfast -g
+$ npm i
+$ superfast
+```
--- client.js
+++ client.js
@@ -3,17 +3,17 @@ var Temple = require("templejs");
 App.ready(function() {
 	var view = Temple.create("todomvc-layout").paint("body");
 
-    App.call("getItems").then(function(res) {
-        res.forEach(function(i) {
-            view.addItem(i.value, i.completed);
-        });
-    }).catch(function(e) {
-        console.error(e);
-    });
+	App.call("getItems").then(function(res) {
+		res.forEach(function(i) {
+			view.addItem(i.value, i.completed);
+		});
+	}).catch(function(e) {
+		console.error(e);
+	});
 
-    Trackr.autorun(function() {
-        App.call("saveItems", view.getItems()).catch(function(e) {
-            console.error(e);
-        });
-    });
+	Trackr.autorun(function() {
+		App.call("saveItems", view.getItems()).catch(function(e) {
+			console.error(e);
+		});
+	});
 });
