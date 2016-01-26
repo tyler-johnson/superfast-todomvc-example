c3fe0bf298046119cbae106d05341ef54243811f
Tyler Johnson <tyler@tylerjohnson.me>
2016-01-26 16:01:21 -0700

    upgraded for superfast 0.2

--- _include.js
+++ _include.js
@@ -1,6 +1,21 @@
 var path = require("path");
+var Temple = require("templejs");
+
+function transform(file, src) {
+	if (path.extname(file.path) !== ".html") return;
+	file.setType("script");
+	file.target("client");
+
+	return [
+		"var Temple = require(\"templejs\");",
+		"module.exports = (function() {",
+		Temple.compile(src),
+		"}).call(this);"
+	].join("\n");
+}
 
 module.exports = function(compile) {
 	var todomvccss = path.relative(__dirname, require.resolve("todomvc-app-css/index.css"));
-	compile.include(todomvccss, [ "client" ]);
+	compile.include(todomvccss, "style", [ "client" ]);
+	compile.transform(transform);
 };
